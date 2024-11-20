9b02d3452a2bccb4297041774e14b22d12d47fea
medousa89 <medousa89@gmail.com>
2024-11-20 12:10:30 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget "https://raw.githubusercontent.com/And73Smi/IPTV-M3U/refs/heads/main/iptv.m3u"
 sed -e "/#EXTM3U/d"  -e "/######/d" -e "s/https/http/g" -e "s/^http:/<uri>https/" -e "s/.*,/<name>/"   "iptv.m3u" > "stream.xml"
-./new.py
+python3 new.py
